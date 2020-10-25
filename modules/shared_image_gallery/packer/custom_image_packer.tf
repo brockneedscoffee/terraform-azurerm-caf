fa6fbc98993bb3a7251d96a4659d15f67cc18b78
Ben <v-beprak@microsoft.com>
2020-10-25 22:31:17 +0800

    move null-resource to root module

--- modules/shared_image_gallery/packer/custom_image_packer.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "packer-exec" {
-  count = var.settings.use_packer ? 1 : 0
-  provisioner "local-exec" {
-    command = "packer build -var-file=${var.settings.packer_file_path} ${var.settings.packer_configuration_file_path}"
-  }
-}
